79a82c7a169e2e5ab61bc6dcb2030d588e0d31eb
BadCompanySCO <145200812+BadCompanySCO@users.noreply.github.com>
2023-11-25 20:31:21 +0000

    version2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st  # pip install streamlit
 import pandas as pd  # pip install pandas
-import plotly.express as px  # pip install plotly-express
 import base64  # Standard Python Module
 from io import StringIO, BytesIO  # Standard Python Module
 from streamlit_option_menu import option_menu
